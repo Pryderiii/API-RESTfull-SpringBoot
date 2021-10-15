d99adbbacbd5a4651e6250b5e0f61ae659547873
Francisco San Martin <fra.smartin@outlook.com>
2021-10-15 18:37:26 -0300

    Commit inicial

---
+++ src/main/java/com/projects/BCIprueba/BciPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.projects.BCIprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BciPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BciPruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/BCIprueba/BciPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.BCIprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BciPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
